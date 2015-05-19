405cdebe61cefbecaf094167d7bc6c9883d71141
robregonm <robregonm@gmail.com>
2015-05-19 14:12:09 -0500

    Agregada regla de url para editar pregunta/respuesta

--- config/urls.php
+++ config/urls.php
@@ -8,6 +8,7 @@ return [
     'pr/tag/<tags>' => 'qa/default/tags',
     'pr/sugerir-tag' => 'qa/default/tag-suggest',
     'pr/<alias>-<id>' => 'qa/default/view',
+    'pr/editar/<id>' => 'qa/default/edit',
     'noticias' => 'news/view',
     'user' => 'user/view',
     'sobre-yii' => 'site/about',
